f9c5c6b93caba998a8d4d304c0fd21f4dd8f5f43
sychyow <sychyow@outlook.com>
2018-09-16 00:00:50 +0300

    Second activity added and called

--- app/src/main/java/org/supremus/sych/sender/MainActivity.java
+++ app/src/main/java/org/supremus/sych/sender/MainActivity.java
@@ -2,12 +2,24 @@ package org.supremus.sych.sender;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText etMsg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnPreview = findViewById(R.id.btn_preview);
+        btnPreview.setOnClickListener(this);
+        etMsg = findViewById(R.id.msg_text);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String theMessage = etMsg.getText().toString();
+        PreviewActivity.launch(this, theMessage);
     }
 }
--- app/src/main/java/org/supremus/sych/sender/PreviewActivity.java
+++ app/src/main/java/org/supremus/sych/sender/PreviewActivity.java
@@ -1,13 +1,29 @@
 package org.supremus.sych.sender;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PreviewActivity extends AppCompatActivity {
+
+    private final static String MSG = "MSG";
+
+    TextView tvPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
+        tvPreview = findViewById(R.id.tv_preview);
+        String theMessage = this.getIntent().getExtras().getString(MSG);
+        tvPreview.setText(theMessage);
+    }
+
+    public static void launch(Activity parent, String textMessage) {
+        final Intent intent = new Intent(parent, PreviewActivity.class);
+        intent.putExtra(MSG, textMessage);
+        parent.startActivity(intent);
     }
 }
